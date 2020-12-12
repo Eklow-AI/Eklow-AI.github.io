e2975ffd0386a3e9509771418c96e11991dbf31f
Daniel Lobaton <dlobaton@eklow.ai>
2020-12-12 17:23:36 -0500

    Added registration

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
-$( document ).ready(function() {
-    
+$(document).ready(function(){
+    $("#RegisterEmail").click(() => {
+        let email = document.getElementById('recipient-email').value;
+        const settings = {
+            body: JSON.stringify({email}),
+            headers: new Headers({"content-type": "application/json"}),
+            method: "POST"
+        }
+        fetch(`https://waitinglistserv.herokuapp.com/registerEmail`, settings)
+        $("#subscribeModal").removeClass("show");
+        $("#subcribeModal").css({
+            "display": "none;"
+        })
+        $("subcribeModal").attr("aria-hidden","true");
+        $("#top").removeClass("modal-open");
+        $(".modal-backdrop").remove();
+    })
 });
